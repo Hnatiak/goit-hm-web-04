f9f0c0af3746fb7956130d350177f2b2233d7c39
Hnatiak <58102343+Hnatiak@users.noreply.github.com>
2024-02-15 12:43:26 +0100

    Changes

---
+++ Dockerfile
@@ -0,0 +1,29 @@
+# Use an official Python runtime as a base image
+FROM python:3.11-slim
+
+# Set the working directory to /app
+ENV APP_HOME /app
+
+WORKDIR $APP_HOME
+
+# Copy the current directory contents into the container at /app
+COPY pyproject.toml $APP_HOME/pyproject.toml
+COPY poetry.lock $APP_HOME/poetry.lock
+
+RUN pip install poetry
+RUN poetry config virtualenvs.create false && poetry install --only main
+# Install any needed packages specified in requirements.txt
+# RUN pip install --upgrade pip setuptools
+# RUN pip install --no-cache-dir -r requirements.txt
+# RUN pip install Flask  # Add this line to install Flask
+
+COPY . .
+
+# Make port 80 available to the world outside this container
+EXPOSE 3000
+
+# Define environment variable
+ENV NAME PersonalAssistant
+
+# Run main.py when the container launches
+CMD ["python", "main.py"]
--- main.py
+++ main.py
@@ -6,15 +6,14 @@ import mimetypes
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from threading import Thread
 import socket
-from jinja2 import Environment, FileSystemLoader
+from datetime import datetime
 
 BASE_DIR = Path()
 BUFFER_SIZE = 1024
-HTTP_PORT = 8080
+HTTP_PORT = 3000
 HTTP_HOST = '0.0.0.0'
 SOCKET_HOST = '127.0.0.1'
-SOCKET_PORT = 4000
-jinja = Environment(loader=FileSystemLoader('templates'))
+SOCKET_PORT = 5000
 
 class GoitFramework(BaseHTTPRequestHandler):
 
@@ -41,7 +40,7 @@ class GoitFramework(BaseHTTPRequestHandler):
         client_socket.close()
 
         self.send_response(302)
-        self.send_header('Location', '/contact')
+        self.send_header('Location', '/message')
         self.end_headers()
 
     def send_html(self, filename, status_code=200):
@@ -50,18 +49,6 @@ class GoitFramework(BaseHTTPRequestHandler):
         self.end_headers()
         with open(filename, 'rb') as file:
             self.wfile.write(file.read())
-
-    def render_template(self, filename, status_code=200):
-        self.send_response(status_code)
-        self.send_header('Content-Type', 'text/html')
-        self.end_headers()
-
-        with open('storage/data.json', 'r', encoding='utf-8') as file:
-            data = json.load(file)
-
-        template = jinja.get_template(filename)
-        html = template.render(blogs=data)
-        self.wfile.write(html.encode())
             
     def send_static(self, filename, status_code=200):
         self.send_response(status_code)
@@ -77,7 +64,7 @@ class GoitFramework(BaseHTTPRequestHandler):
 def save_data_from_form(data):
     parse_data = urllib.parse.unquote_plus(data.decode())
     try: 
-        parse_dict = {key: value for key, value in [el.split('=') for el in parse_data.split('&')]}
+        parse_dict = {datetime.now().isoformat(): {key: value for key, value in [el.split('=') for el in parse_data.split('&')]}}
         with open('storage/data.json', 'w', encoding='utf-8') as file:
             json.dump(parse_dict, file, ensure_ascii=False, indent=4)
     except ValueError as err:
@@ -88,7 +75,7 @@ def save_data_from_form(data):
 def run_socket_server(host, port):
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     server_socket.bind((host, port))
-    logging.info('Starting socket server')
+    logging.info('Starting UDP socket server')
     try:
         while True:
             msg, address = server_socket.recvfrom(BUFFER_SIZE)
